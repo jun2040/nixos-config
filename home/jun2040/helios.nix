1147b0c00b0e41de8c98f63e7ce50c8ab091ac7e
jun2040 <isy.junny@gmail.com>
2024-10-11 01:39:12 +0200

    Configure waybar clock

--- home/jun2040/helios.nix
+++ home/jun2040/helios.nix
@@ -71,6 +71,23 @@
 
   programs.waybar = {
     enable = true;
+    style = ''
+      * {
+        font-family: 'Fira Code';
+      }
+    '';
+    settings =[{
+      height = 30;
+      layer = "top";
+      position = "top";
+      modules-right = [
+        "clock"
+      ];
+      clock = {
+        interval = 1;
+        format = "  {:%H:%M:%S\n%Y\/%m\/%d}";
+      };
+    }];
   };
 
   wayland.windowManager.hyprland = {
