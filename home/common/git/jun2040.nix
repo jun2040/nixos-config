f1b444686776838956cc3706ea53a32d4424f989
jun2040 <isy.junny@gmail.com>
2024-10-15 12:05:01 +0200

    Resolve merge conflict on git module

--- home/common/git/jun2040.nix
+++ home/common/git/jun2040.nix
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-{}:
->>>>>>> a88944d94d87d6948c9f01169ed20d8e8d7b83b3
 {
   programs.git = {
     enable = true;
@@ -10,10 +6,7 @@
     extraConfig = {
       init.defaultBranch = "main";
       safe.directory = "/etc/nixos";
-<<<<<<< HEAD
       pull.rebase = false;
-=======
->>>>>>> a88944d94d87d6948c9f01169ed20d8e8d7b83b3
     };
   };
 }
