a14bfac4a756f5f0696676579b786ea83cdd0c8a
jun2040 <isy.junny@gmail.com>
2024-11-04 16:32:17 +0100

    Add battery module to waybar

--- home/common/hyprland/waybar.nix
+++ home/common/hyprland/waybar.nix
@@ -6,16 +6,29 @@
         font-family: 'Fira Code';
       }
     '';
-    settings =[{
+    settings = [{
       height = 30;
       layer = "top";
       position = "top";
       modules-right = [
+        "battery"
         "clock"
       ];
       clock = {
         interval = 1;
         format = "  {:%H:%M:%S\n%Y\/%m\/%d}";
+      };
+      battery = {
+        states = {
+	  warning = 30;
+	  critical = 15;
+	};
+	format = "{capacity}% {icon}";
+	format-full = "{capacity}% {icon}";
+        format-charging = "{capacity}% ";
+        format-plugged = "{capacity}% ";
+        format-alt = "{time} {icon}";
+	format-icons = [ " " " " " " " " " " ];
       };
     }];
   };
