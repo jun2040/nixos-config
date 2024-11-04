1e594b3461d3da75161fa1e295ca2db7071f1cfe
jun2040 <isy.junny@gmail.com>
2024-11-04 17:38:46 +0100

    Add workspaces

--- home/common/hyprland/hyprland.nix
+++ home/common/hyprland/hyprland.nix
@@ -3,7 +3,15 @@
     ./waybar.nix
   ];
 
-  wayland.windowManager.hyprland = {
+  wayland.windowManager.hyprland = 
+  let
+    go_to_workspace = x: [ "$mod, ${toString x}, workspace, ${toString x}" ];
+    move_to_workspace = x: [ "$mod SHIFT, ${toString x}, movetoworkspace, ${toString x}" ];
+    loop = i: n: template:
+      if i < n
+      then (template i) ++ (loop (i + 1) n template)
+      else (template i);
+  in {
     enable = true;
     settings = {
       "$mod" = "SUPER";
@@ -21,7 +29,9 @@
 	"$mod SHIFT, L, movewindow, r"
 	"$mod SHIFT, K, movewindow, u"
 	"$mod SHIFT, J, movewindow, d"
-      ];
+      ]
+      ++ (loop 1 9 go_to_workspace)
+      ++ (loop 1 9 move_to_workspace);
       misc = {
         force_default_wallpaper = 0;
 	disable_hyprland_logo = true;
