f53d28ae92636049f12d6c090dee5b5afb177827
jun2040 <isy.junny@gmail.com>
2024-11-26 10:59:12 +0100

    Remove redundant dev shell

--- flake.nix
+++ flake.nix
@@ -87,27 +87,5 @@
         ];
       };
     };
-
-    devShells = ({ pkgs }: {
-      c = pkgs.mkShell.override
-        {
-          # Override stdenv in order to change compiler:
-          # stdenv = pkgs.clangStdenv;
-        }
-        {
-          packages = with pkgs; [
-            clang-tools
-            cmake
-            codespell
-            conan
-            cppcheck
-            doxygen
-            gtest
-            lcov
-            vcpkg
-            vcpkg-tool
-          ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
-        };
-    });
   };
 }
